015774ef3b72c3684f41036df9c16e67ef121868
Neytullah BAHADIR <nate-nate86@hotmail.com>
2022-01-01 17:13:25 +0100

    integrated strip into app and updating favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'ppk_test_51KD90cC301ohQnKDAGNT4X6RsfD9N59DSXwM7P0EI5Wq8vePx1L6UXoATlyjH4CFD2ddbAIdobnw7IWFKVqb85g600NLAVSQhB';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Fashion Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
